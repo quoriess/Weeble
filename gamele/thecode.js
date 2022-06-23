8c0fdcd8740428fe96440afa9bc2949106ff7c1d
quoriess <35757459+quoriess@users.noreply.github.com>
2022-06-23 15:15:51 +0100

    gamele

---
+++ gamele/thecode.js
@@ -0,0 +1,120 @@
+function getCurrentDate() {
+  const t = new Date();
+  const date = ('0' + t.getUTCDate()).slice(-2);
+  const month = ('0' + (t.getUTCMonth() + 1)).slice(-2);
+  const year = t.getUTCFullYear();
+  return date+"/"+month+"/"+year;
+}
+function convertUTCDateToLocalDate(date) {
+    var newDate = new Date(date.getTime() - date.getTimezoneOffset()*60*1000);
+    return newDate;   
+}
+function cookieDate(d) {
+  function d2(n) { return n < 10 ? '0' + n : n; }
+  var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+  return '' +
+    days[d.getUTCDay()] + ', ' +
+    d2(d.getUTCDate()) + '-' +
+    months[d.getUTCMonth()] + '-' +
+    d.getUTCFullYear() + ' ' +
+    d2(d.getUTCHours()) + ':' +
+    d2(d.getUTCMinutes()) + ':' +
+    d2(d.getUTCSeconds()) + ' GMT';
+}
+function setCookie(cname, cvalue) {
+  var nw=new Date();
+  var f=new Date(nw.getUTCFullYear(),nw.getUTCMonth(),nw.getUTCDate()+1);
+  let expires = "expires="+f.toString()+" GMT";
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function load(){
+	var s=1;
+	while(getCookie("g"+s)!=""){
+		nextClue();
+		var ne = document.getElementById('g' + s);
+		ne.textContent=getCookie("g"+s)=="___"?"":getCookie("g"+s);
+		if(getCookie("g"+s)==keyNow){
+			$("#g"+s).css("background-color","green");
+			tries=clueNo-1;
+			while(clueNo<=6)nextClue();		
+			$(".hiddenBox").html("You guessed it right! <button style='float:right' onclick='copyToCB()'>Share</button>");
+			$(".hiddenBox").css("display","block");
+			return;
+		}
+		$("#g"+s).css("background-color","red");
+		if(clueNo==6){
+			document.getElementById("enter").disabled = true;
+			$(".hiddenBox").html("You failed! The answer was: <b>"+ keyNow+"</b>");
+			$(".hiddenBox").css("display","block");
+		}
+		s+=1;
+	}
+}
+var rngGen=new Math.seedrandom(getCurrentDate());
+function getRandomInt(min,max) {
+  return min+Math.floor(rngGen() * (max-min));
+}
+var shuffled=[]
+var clueNo=0;
+function generateTileOrder(){
+	var alls=[];
+	for(var i=0;i<GRID_SIZE*GRID_SIZE;i++){
+		alls.push(i);
+	}
+	var s1 = alls
+	  .map(value => ({ value, sort: rngGen() }))
+	  .sort((a, b) => a.sort - b.sort)
+	  .map(({ value }) => value);
+	var ars=[]
+	for(var i=0;i<GRID_SIZE/2+GRID_SIZE%2;i++){
+		ars.push([])
+	}
+	for(var r=0;r<GRID_SIZE*GRID_SIZE;r++){
+		var i=s1[r];
+		var x=Math.floor(i/GRID_SIZE);
+		var y=i%GRID_SIZE;
+		var fValue=Math.min(Math.min(x,GRID_SIZE-1-x),Math.min(y,GRID_SIZE-1-y))
+		ars[fValue].push(i);
+	}
+	//hardCode for grid size=6
+	//20
+	//12
+	//4
+	var vls=[[6,0,0],[4,2,0],[3,3,0],[2,4,1],[3,2,1],[2,1,2]];
+	var cumSums=[0,0,0];
+	for(var i=0;i<GRID_SIZE; i++){
+		for(var j=0;j<3;j++){
+			for(var k=0;k<vls[i][j];k++){
+				shuffled.push(ars[j][cumSums[j]]);
+				cumSums[j]+=1;
+			}
+		}
+	}
+}
+var steps=[3,4,6,6,6,11]
+var curRevealed=0;
+function nextClue(){
+	for(var j=0;j<steps[clueNo];j++){
+		$("#t"+shuffled[curRevealed]).fadeOut();
+		curRevealed+=1;
+	}
+	clueNo+=1;
+}
---
+++ gamele/uiscript.js
@@ -0,0 +1,93 @@
+var keyNow=0;
+var GRID_SIZE=6;
+var uiKeys=[]
+$(function() {
+    $("#txtbox").keyup(function(event) {
+            if (event.keyCode === 13) {
+                $("#enter").click();
+            }
+        }
+    );
+    $("#txtbox").focus();
+	uiKeys=keys.map((x)=>x);
+	uiKeys.push("");
+    $("#txtbox").autocomplete({
+        source: uiKeys
+    });
+	$( function() {
+		$( "#dialog" ).dialog({autoOpen:false});
+	} );
+	
+	$("#rel-container").append("<img id='theImage' src='406163.jpg'/>");
+	for(var i=0;i<GRID_SIZE;i++){
+		for(var j=0;j<GRID_SIZE;j++){
+			var gid=i*GRID_SIZE+j;
+			$("#rel-container").append("<div id='t"+gid+"' class='tile'></div>");
+			$("#t"+gid).css("top",(100*i)/GRID_SIZE+"%");
+			$("#t"+gid).css("left",(100*j)/GRID_SIZE+"%");
+		}	
+	}
+	var f=100/GRID_SIZE;
+	$(".tile").css("width",f+"%")
+	$(".tile").css("height",f+"%")
+	$(".tile").css("position","absolute");
+	generateTileOrder();
+	var arm=new Math.seedrandom("amogus");
+	var t1=new Date(2022,3,30);
+	var nw=new Date();
+	var utct1=new Date(t1.getUTCFullYear(),t1.getUTCMonth(),t1.getUTCDate());
+	var utcd=new Date(nw.getUTCFullYear(),nw.getUTCMonth(),nw.getUTCDate())
+	var dayNo= (Math.floor((utcd-utct1)/(24*3600*1000))+1);
+	console.log(dayNo);
+	var shuffledKeys = keys
+	  .map(value => ({ value, sort:  arm() }))
+	  .sort((a, b) => a.sort - b.sort)
+	  .map(({ value }) => value);
+	keyNow=shuffledKeys[dayNo%70];
+	$("#theImage").attr("src",data[keyNow]);
+	nextClue();
+	load();
+});
+function showFaqs(){
+	$("#dialog").dialog('open');
+	
+}
+var tries=0;
+function copyToCB(){
+	var t1=new Date(2022,5,22);
+	var nw=new Date();
+	var utct1=new Date(t1.getUTCFullYear(),t1.getUTCMonth(),t1.getUTCDate());
+	var utcd=new Date(nw.getUTCFullYear(),nw.getUTCMonth(),nw.getUTCDate())
+	var dayNo= (Math.floor((utcd-utct1)/(24*3600*1000))+1);
+	navigator.clipboard.writeText("Gamele #"+dayNo+": \n"+"\uD83D\uDFE5".repeat(tries-1)+"\uD83D\uDFE9");
+}
+function valueEntered() {
+    if (clueNo == 6) return;
+
+    var ne = document.getElementById('g' + clueNo);
+    var txtbox = document.getElementById('txtbox');
+    if (!uiKeys.includes(txtbox.value)) {
+        alert("not a valid weebshit");
+        return;
+    }
+    ne.textContent = txtbox.value;
+	setCookie("g"+clueNo,txtbox.value==""?"___":txtbox.value);
+    if(txtbox.value==keyNow){
+		tries=clueNo;
+		$("#g"+clueNo).css("background-color","green");	
+		while(clueNo<=6)nextClue();		
+		$(".hiddenBox").html("You guessed it right! <button style='float:right' onclick='copyToCB()'>Share</button>");
+		$(".hiddenBox").css("display","block");
+	}
+	else{
+		$("#g"+clueNo).css("background-color","red");
+		nextClue();
+		if (clueNo == 6) {
+			document.getElementById("enter").disabled = true;
+			$(".hiddenBox").html("You failed! The answer was: <b>"+ keyNow+"</b>");
+			$(".hiddenBox").css("display","block");
+		}
+	}
+	txtbox.value = "";
+    txtbox.focus();
+}
